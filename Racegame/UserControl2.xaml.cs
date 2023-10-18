42db29fe7e3b8d040023847c5c301e7ce14eb690
Skyprop <Yoerix8@gmail.com>
2023-10-18 19:39:00 +0200

    added information screen

--- Racegame/UserControl2.xaml.cs
+++ Racegame/UserControl2.xaml.cs
@@ -20,9 +20,32 @@ namespace Racegame
     /// </summary>
     public partial class UserControl2 : UserControl
     {
+        string[] descriptions = {
+        "Beschrijving van het spel \n" +
+        "placeholder 1",
+
+        "placeholder 2",
+
+        "placeholder 3"
+        };
+
+        int index = 0;
         public UserControl2()
         {
             InitializeComponent();
+            DescriptionBox.Text = descriptions[index];
+        }
+
+        private void Left_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (index != 0) { index--; } else { index = descriptions.Length-1; }
+            DescriptionBox.Text = descriptions[index];
+        }
+
+        private void Right_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (index != descriptions.Length-1) { index++; } else { index = 0; }
+            DescriptionBox.Text = descriptions[index];
         }
     }
 }
