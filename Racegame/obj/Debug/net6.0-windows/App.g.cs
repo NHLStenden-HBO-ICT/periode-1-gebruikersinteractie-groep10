3fb19d6f999e22117af24ccbcdf8b3f8964ab51a
Sytze Dalstra <sytze.dalstra1@student.nhlstenden.nl>
2023-10-31 09:55:46 +0100

    Changed the size of the circuit images and changed the button styles.

--- Racegame/obj/Debug/net6.0-windows/App.g.cs
+++ Racegame/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Racegame {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Racegame {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             Racegame.App app = new Racegame.App();
             app.InitializeComponent();
--- Racegame/obj/Debug/net6.0-windows/UserControl2.g.i.cs
+++ Racegame/obj/Debug/net6.0-windows/UserControl2.g.i.cs
@@ -55,13 +55,13 @@ namespace Racegame {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Racegame;V1.0.0.0;component/usercontrol2.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Racegame;component/usercontrol2.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\UserControl2.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace Racegame {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
